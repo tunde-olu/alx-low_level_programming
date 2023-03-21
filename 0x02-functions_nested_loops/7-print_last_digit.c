e2002c6a3aaa3985d3119b6fe22294ca0b79b580
tunde-olu <tundeolu24@gmail.com>
2023-03-21 21:35:58 +0100

    mandatory task done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,16 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n *= -1;
+		n = -(n % 10);
 	}
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	else if (n > 0)
+	{
+		n = n % 10;
+	}
+	else
+	{
+		n = 0;
+	}
+	_putchar(n + '0');
+	return (n);
 }
-
