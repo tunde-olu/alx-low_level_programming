62d8e77f3fa90b4468fa2ce82aaca8278dbea450
tunde-olu <tundeolu24@gmail.com>
2023-03-21 21:44:27 +0100

    mandatory task done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 98)
 	{
@@ -24,18 +24,18 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (j = n; j <= 98; j++)
 		{
-			printf("%d, ", i);
-			if (i == 98)
+			printf("%d, ", j);
+			if (j == 98)
 			{
-				printf("%d\n", i);
+				printf("%d\n", j);
 			}
 		}
 	}
 	else
 	{
-		printf("%d\n", i);
+		printf("%d\n", n);
 	}
 }
 
