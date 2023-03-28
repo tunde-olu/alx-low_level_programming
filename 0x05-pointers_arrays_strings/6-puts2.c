92316ab9d4b98234291c2578be2414df327520a0
tunde-olu <tundeolu24@gmail.com>
2023-03-28 02:39:04 +0100

    pointers task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,35 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * puts2 - print every other character of a string
  * @str: parameter
  */
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 
 	_putchar('\n');
 }
