733d03c83935db6ec2a2623bd057b47ff58a2e28
tunde-olu <tundeolu24@gmail.com>
2023-03-30 12:01:44 +0100

    more pointers mandatory

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
