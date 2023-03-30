72a90435d4666fff71dfb8f4d65393f07659712a
tunde-olu <tundeolu24@gmail.com>
2023-03-30 13:21:48 +0100

    second commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - compares two strings
- * @s: string parameter
- * Return: int
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
  * _strcmp - appends the src string to the dest string
  * @s1: string
  * @s2: string
@@ -28,23 +9,11 @@ int _strlen(char *s)
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int cmp = 0;
-	int s1_len = _strlen(s1);
-	int s2_len = _strlen(s2);
-
-	for (i = 0; i < s1_len || i < s2_len; i++)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-		{
-			cmp = s1[i] - s2[i];
-		}
-		if (s1[i] == '\0' || s2[i] == '\0')
-		{
-			break;
-		}
+		s1++;
+		s2++;
 	}
-
-	return (cmp);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
