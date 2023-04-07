9e320545c91c8dee89a512b94f6576b23f633829
tunde-olu <tundeolu24@gmail.com>
2023-04-07 16:27:13 +0100

    0x0A. C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,13 +13,15 @@
 
 int main(int argc, char *argv[])
 {
+	int cents, coins = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]), coins = 0;
+	cents = atoi(argv[1]);
 
 	while (cents > 0)
 	{
