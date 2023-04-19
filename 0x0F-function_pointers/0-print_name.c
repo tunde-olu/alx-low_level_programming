11c4be6bb1835ea14353544084bb47fb0a10b3f2
tunde-olu <tundeolu24@gmail.com>
2023-04-19 22:20:05 +0100

    0x0F. C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * print_name - A function that prints a name
  * @name: name to print
@@ -11,3 +13,4 @@ void print_name(char *name, void (*f)(char *))
 
 	f(name);
 }
+
