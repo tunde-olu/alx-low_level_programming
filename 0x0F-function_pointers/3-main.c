a80f5d5881b31e5cac1ed801bd270a3c2b3100ab
tunde-olu <tundeolu24@gmail.com>
2023-04-20 02:13:42 +0100

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	op = argv[2];
 
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
